d04d9a7f8d5a17e12ef913617ef3c0367240d04f
Jason <meyerjaw@gmail.com>
2026-06-04 17:48:32 -0400

    Continuing to work on version upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.getmecookies.licenseplatequest"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.getmecookies.licenseplatequest"
